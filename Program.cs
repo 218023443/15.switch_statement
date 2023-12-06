f9ff8b618139daf7fcf653615e882ce7f1000d58
A252557 <A252557@LSBSA-3WCCHW3>
2023-12-06 14:52:08 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.switch_statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number from 1-3");
+            int number =int.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 1:
+                    Console.WriteLine("you entered '1'");
+                    break;
+                case 2:
+                    Console.WriteLine("you enterd '2'");
+                    break;
+                case 3:
+                    Console.WriteLine("you entered '3'");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input. please enter between 1 and 3");
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
